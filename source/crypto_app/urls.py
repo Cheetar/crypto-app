d4a6aa5e5f5fb75fb5fa061cd8014b80c1acf55c
Grzegorz Uriasz <gorbak25@gmail.com>
2018-05-02 03:33:31 +0200

    App ready for deployment

--- source/crypto_app/urls.py
+++ source/crypto_app/urls.py
@@ -12,9 +12,12 @@ urlpatterns = [
     path("login", views.login),
     path("newcipher", views.newcipher),
     path("edit_python_cipher/<int:cipher_id>", views.edit_python_cipher),
+    path("edit_js_cipher/<int:cipher_id>", views.edit_js_cipher),
+    path("edit_cpp_cipher/<int:cipher_id>", views.edit_cpp_cipher),
     path("execute_python_cipher/<int:cipher_id>", views.execute_python_cipher),
-    path("play_with_cipher/", views.play_with_cipher),
-    path("edit_cpp_cipher/<int:cipher_id>", views.edit_cpp_cipher)
+    path("execute_js_cipher/<int:cipher_id>", views.execute_js_cipher),
+    path("execute_cpp_cipher/<int:cipher_id>", views.execute_cpp_cipher),
+    path("play_with_cipher/", views.play_with_cipher)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- source/crypto_app/views.py
+++ source/crypto_app/views.py
@@ -10,6 +10,11 @@ import sys
 import os
 import json
 import prctl
+import subprocess
+import signal
+import random
+import string
+from py_mini_racer import py_mini_racer
 
 USER_ID = "user_id"
 AUTHENTICATED = "authenticated"
@@ -41,7 +46,7 @@ def index(request):
     js_ciph = list(map(lambda o: [o.name, o.author.nick, "/execute_js_cipher/{}".format(o.id)],
                        JSCipher.objects.filter(isReady=True)))
     cpp_ciph = list(
-        map(lambda o: [o.name, "/execute_cpp_cipher/{}".format(o.id)],
+        map(lambda o: [o.name, o.author.nick, "/execute_cpp_cipher/{}".format(o.id)],
             CPPCipher.objects.filter(isReady=True)))
 
     public_ciphs = py_ciph + js_ciph + cpp_ciph
@@ -142,11 +147,11 @@ def newcipher(request):
             cipher.save()
             return HttpResponseRedirect("/edit_python_cipher/{}".format(cipher.id))
         elif language == "js":
-            cipher = JSCipher.objects.create(author=person, name=name, isReady=False, encryptFun="", decryptFun="")
+            cipher = JSCipher.objects.create(author=person, name=name, isReady=False, encryptFun="var enc = (text, key) =>\n{\n}\n", decryptFun="var dec = (cipher, key) =>\n{\n}\n")
             cipher.save()
             return HttpResponseRedirect("/edit_js_cipher/{}".format(cipher.id))
         elif language == "cpp":
-            cipher = CPPCipher.objects.create(author=person, name=name, isReady=False, programCode=b"")
+            cipher = CPPCipher.objects.create(author=person, name=name, isReady=False, programCode="#include <string>\n\nusing namespace std;\n\nint main(int argc, char** argv)\n{\n    string op = string(argv[1]);\n    string arg = string(argv[2]);\n    string key = string(argv[3]);\n    return 0;\n}\n")
             cipher.save()
             return HttpResponseRedirect("/edit_cpp_cipher/{}".format(cipher.id))
         else:
@@ -193,6 +198,43 @@ def edit_python_cipher(request, cipher_id):
 
 @never_cache
 @csrf_exempt
+def edit_js_cipher(request, cipher_id):
+    logged_in = request.session.get(AUTHENTICATED)
+    if logged_in == None:
+        logged_in = False
+    else:
+        logged_in = bool(logged_in)
+
+    if not logged_in:
+        return HttpResponseRedirect("/")
+
+    uuid = request.session[USER_ID]
+    person = Partipicant.objects.get(uuid=uuid)
+
+    try:
+        cipher = JSCipher.objects.get(id=cipher_id, author=person)
+    except:
+        return HttpResponseRedirect("/")
+
+    if request.method == "POST":
+
+        tmp = request.POST["enc"].lstrip().split("\n")
+        tmp[0] = "var enc = (text, key) =>"
+        cipher.encryptFun = '\n'.join(tmp)
+
+        tmp = request.POST["dec"].lstrip().split("\n")
+        tmp[0] = "var dec = (cipher, key) =>"
+        cipher.decryptFun = '\n'.join(tmp)
+
+        cipher.isReady = request.POST["vis"]=="true"
+        cipher.save(force_update=True)
+
+        return HttpResponse("OK")
+    else:
+        return render(request, "crypto_app/edit_js_cipher.html", {'cipher_name': cipher.name, "enc":cipher.encryptFun, "dec":cipher.decryptFun, "ready":cipher.isReady, "id":cipher.id})
+
+@never_cache
+@csrf_exempt
 def edit_cpp_cipher(request, cipher_id):
     logged_in = request.session.get(AUTHENTICATED)
     if logged_in == None:
@@ -213,14 +255,14 @@ def edit_cpp_cipher(request, cipher_id):
 
     if request.method == "POST":
 
-        tmp = request.POST["code"].lstrip().split("\n")
+        tmp = request.POST["code"]
         cipher.programCode = tmp
         cipher.isReady = request.POST["vis"]=="true"
         cipher.save(force_update=True)
 
         return HttpResponse("OK")
     else:
-        return render(request, "crypto_app/edit_cpp_cipher.html", {'cipher_name': cipher.name, "code":cipher.programCode, "ready":cipher.isReady, "id":cipher.id})
+        return render(request, "crypto_app/edit_cpp_cipher.html", {'cipher_name': cipher.name, "code": cipher.programCode, "ready":cipher.isReady, "id":cipher.id})
 
 
 @csrf_exempt
@@ -264,6 +306,13 @@ def execute_python_cipher(request, cipher_id):
             sys.stderr.close()
             sys.stdin.close()
 
+            # Enforce timeout
+            def timeout_handler(signum, frame):
+                raise Exception("Execution of cipher timed out")
+
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(10)
+
             #Enable seccomp
             #prctl.set_seccomp(True)
 
@@ -272,29 +321,21 @@ def execute_python_cipher(request, cipher_id):
                     res = cipher.getEncFun()(arg, key)
                     if type(res) is not str:
                         w.write(json.dumps({"res": "", "error": "Encryption function returned invalid type! Return type: {}".format(str(type(res)))}).encode('utf-8'))
-                        w.flush()
-                        return 0
-                    w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
-                    #w.flush()
-                    return 0
+                    else:
+                        w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
                 elif op == "dec":
                     res = cipher.getDecFun()(arg, key)
                     if type(res) is not str:
                         w.write(json.dumps({"res": "",
                                              "error": "Decryption function returned invalid type! Return type: {}".format(
                                                  str(type(res)))}).encode('utf-8'))
-                        w.flush()
-                        return 0
-                    w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
-                    w.flush()
-                    return 0
+                    else:
+                        w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
                 else:
-                    return 0
+                    pass
             except SyntaxError as e:
                 err = (''.join(traceback.format_exception_only(type(e), e)))
                 w.write(json.dumps({"res": "", "error": err}).encode('utf-8'))
-                w.flush()
-                return 0
             except NameError as e:
                 (exc_type, exc_value, exc_traceback) = sys.exc_info()
                 tb_list = traceback.extract_tb(exc_traceback)
@@ -302,8 +343,6 @@ def execute_python_cipher(request, cipher_id):
                 err = ''.join(traceback.format_exception_only(type(e), e))
                 err += tb_list.__str__()
                 w.write(json.dumps({"res": "", "error": err}).encode('utf-8'))
-                w.flush()
-                return 0
             except ValueError as e:
                 (exc_type, exc_value, exc_traceback) = sys.exc_info()
                 tb_list = traceback.extract_tb(exc_traceback)
@@ -312,12 +351,12 @@ def execute_python_cipher(request, cipher_id):
                 err += tb_list.__str__()
 
                 w.write(json.dumps({"res": "", "error": err}).encode('utf-8'))
-                w.flush()
-                return 0
             except Exception as e:
                 w.write(json.dumps({"res": "", "error": "Your code broke something:\n" + str(e)}).encode('utf-8'))
-                w.flush()
-                return 0
+
+            signal.alarm(10)
+            w.flush()
+            return 0
         else:
             # Parent
             w.close()
@@ -327,10 +366,214 @@ def execute_python_cipher(request, cipher_id):
                 data = r.read()
                 if not data: break
                 child_data += data
+            if child_data == b"":
+                return JsonResponse({"res":"", "error":"A fatal exception occured"})
             return HttpResponse(child_data.decode("utf-8"))
 
     else:
         return HttpResponseRedirect("/edit_python_cipher/{}".format(cipher_id))
+
+
+@csrf_exempt
+def execute_js_cipher(request, cipher_id):
+    try:
+        cipher = JSCipher.objects.get(id=cipher_id)
+    except:
+        return HttpResponseRedirect("/")
+
+    if cipher.isReady == False:
+
+        logged_in = request.session.get(AUTHENTICATED)
+        if logged_in == None:
+            logged_in = False
+        else:
+            logged_in = bool(logged_in)
+
+        if not logged_in:
+            return HttpResponseRedirect("/")
+
+        uuid = request.session[USER_ID]
+        person = Partipicant.objects.get(uuid=uuid)
+
+        if cipher.author != person:
+            return HttpResponseRedirect("/")
+
+    if request.method == "POST":
+        op = request.POST["op"]
+        arg = request.POST["arg"]
+        key = request.POST["key"]
+
+        #Create a secomp sandbox
+        r, w = os.pipe()
+        r, w = os.fdopen(r, 'rb', 0), os.fdopen(w, 'wb', 0)
+
+        newpid = os.fork()
+        if newpid == 0:
+            # Child
+            r.close()
+            sys.stdout.close()
+            sys.stderr.close()
+            sys.stdin.close()
+
+            #Enforce timeout
+            def timeout_handler(signum, frame):
+                raise Exception("Execution of cipher timed out")
+
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(10) #10 seconds
+
+            #Enable seccomp
+            #prctl.set_seccomp(True)
+
+            try:
+                ctx = py_mini_racer.MiniRacer()
+                res = None
+                if op == "enc":
+                    ctx.eval(cipher.encryptFun)
+                    res = ctx.call("enc", arg, key)
+                elif op == "dec":
+                    ctx.eval(cipher.decryptFun)
+                    res = ctx.call("dec", arg, key)
+
+                if type(res) is not str:
+                    w.write(json.dumps({"res": "",
+                                        "error": "Function returned invalid type! Return type: {}".format(
+                                            str(type(res)))}).encode('utf-8'))
+                else:
+                    w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
+
+            except Exception as e:
+                w.write(json.dumps({"res": "", "error": "Your code broke something:\n" + str(e)}).encode('utf-8'))
+
+            w.flush()
+            signal.alarm(0)
+            return 0
+
+        else:
+            # Parent
+            w.close()
+            child_data = b""
+            # Read from the child until the child is killed
+            while 1:
+                data = r.read()
+                if not data: break
+                child_data += data
+            if child_data == b"":
+                return JsonResponse({"res":"", "error":"A fatal exception occured"})
+            return HttpResponse(child_data.decode("utf-8"))
+
+    else:
+        return HttpResponseRedirect("/edit_js_cipher/{}".format(cipher_id))
+
+@csrf_exempt
+def execute_cpp_cipher(request, cipher_id):
+    try:
+        cipher = CPPCipher.objects.get(id=cipher_id)
+    except:
+        return HttpResponseRedirect("/")
+
+    if cipher.isReady == False:
+
+        logged_in = request.session.get(AUTHENTICATED)
+        if logged_in == None:
+            logged_in = False
+        else:
+            logged_in = bool(logged_in)
+
+        if not logged_in:
+            return HttpResponseRedirect("/")
+
+        uuid = request.session[USER_ID]
+        person = Partipicant.objects.get(uuid=uuid)
+
+        if cipher.author != person:
+            return HttpResponseRedirect("/")
+
+    if request.method == "POST":
+        op = request.POST["op"]
+        arg = request.POST["arg"]
+        key = request.POST["key"]
+
+        #Create a secomp sandbox
+        r, w = os.pipe()
+        r, w = os.fdopen(r, 'rb', 0), os.fdopen(w, 'wb', 0)
+
+        newpid = os.fork()
+        if newpid == 0:
+            # Child
+            r.close()
+            sys.stdout.close()
+            sys.stderr.close()
+            sys.stdin.close()
+
+            #Enforce timeout
+            def timeout_handler(signum, frame):
+                raise Exception("Execution of cipher timed out")
+
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(10) #10 seconds
+
+            #Enable seccomp
+            #prctl.set_seccomp(True)
+
+            try:
+                #compile the cipher
+                random.seed()
+                rand_name = ''.join([random.choice(string.digits) for _ in range(10)])
+                with open("/tmp/"+rand_name+".cpp", "w") as f:
+                    f.write(cipher.programCode)
+
+                p = subprocess.Popen(['g++', '-std=c++11', '-O3', '-o', '/tmp/'+rand_name, '/tmp/'+rand_name+".cpp"],
+                          stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                return_code = p.wait()
+                err = p.stdout.read()+p.stderr.read()
+                err = err.decode('utf-8')
+                err = err.replace("/tmp/"+rand_name+".cpp:", "program.cpp:")
+
+                #remove program source
+                os.remove("/tmp/"+rand_name+".cpp")
+
+                if return_code != 0:
+                    w.write(json.dumps({"res": "", "error": err}).encode('utf-8'))
+                else:
+                    if op == "enc" or op == "dec":
+                        p = subprocess.Popen(
+                            ['/tmp/' + rand_name, op, arg, key],
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+                        return_code = p.wait()
+                        os.remove("/tmp/"+rand_name)
+                        if return_code == 0:
+                            res = p.stdout.read().decode('utf-8')
+                            w.write(json.dumps({"res": res, "error": ""}).encode('utf-8'))
+                        else:
+                            err = p.stderr.read().decode('utf-8')
+                            w.write(json.dumps({"res": "", "error": "Program execution failed. Exit code was {}. STDERR was {}".format(return_code, err)}).encode('utf-8'))
+                    else:
+                        pass
+            except Exception as e:
+                w.write(json.dumps({"res": "", "error": "Your code broke something:\n" + str(e)}).encode('utf-8'))
+
+            w.flush()
+            signal.alarm(0)
+            return 0
+
+        else:
+            # Parent
+            w.close()
+            child_data = b""
+            # Read from the child until the child is killed
+            while 1:
+                data = r.read()
+                if not data: break
+                child_data += data
+            if child_data == b"":
+                return JsonResponse({"res":"", "error":"A fatal exception occured"})
+            return HttpResponse(child_data.decode("utf-8"))
+
+    else:
+        return HttpResponseRedirect("/edit_cpp_cipher/{}".format(cipher_id))
 
 @never_cache
 def play_with_cipher(request):
